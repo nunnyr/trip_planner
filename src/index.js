9a593fc50b0587c957409dd7efa628c066bcff49
Nunny R <nunnyreyes@gmail.com>
2017-11-21 11:59:44 -0500

    added map

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Creating trip planner, ready to change")
+console.log("Geography! More notes")
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibnVubnlyZXllcyIsImEiOiJjamE5dWhoZHEwYzBoMndxcGQwYjQ3MG84In0.uEnk5ddZHPfgraGRLvQmZQ';
+
+
+const map = new mapboxgl.Map({
+    container: "map",
+    center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+    zoom: 12, // starting zoom
+    style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+const markerDomEl = document.createElement("div");
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
+
+
+
