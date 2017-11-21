2ff0d3c466adb9013d31e4bdd7be9886c9d068d7
Nunny R <nunnyreyes@gmail.com>
2017-11-21 11:15:49 -0500

    ann

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    //context: __dirname + "/app",
+        entry: "./src/index.js",
+        output: {
+            path: __dirname + "/public",
+            filename: "bundle.js"
+    }
+
+}
